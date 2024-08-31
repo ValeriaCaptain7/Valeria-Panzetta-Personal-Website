195e0084fc1f98db161672d2faf344f693340840
Valeria Panzetta <pantera3072@gmail.com>
2024-08-31 15:06:10 +0200

    upload files

---
+++ languages/script.js
@@ -0,0 +1,208 @@
+const translations = {
+  it: {
+    "home-name": "Ciao! Io sono Valeria Panzetta",
+    "home-title": "Scopri la Nuova Idea di Business",
+    "home-p1":
+      "Innovazione e sviluppo per far crescere la tua impresa. Scopri come posso aiutarti a raggiungere i tuoi obiettivi.",
+    "home-cta": "Esplora il mio Portfolio",
+    "home-business": "Consulenza Digital Branding",
+    "home-strategies":
+      "Strategie innovative per portare la tua idea al successo.",
+    "home-development": "Sviluppo Web",
+    "home-solutions": "Soluzioni digitali all'avanguardia per la tua azienda.",
+    "home-marketing": "Marketing Digitale",
+    "home-campaigns":
+      "Cattura l'attenzione del tuo pubblico con campagne efficaci.",
+    "about-title": "Chi Sono",
+    "about-p1":
+      "Sono un professionista con anni di esperienza nel settore, specializzato in sviluppo web e design front-end.",
+    "about-p2":
+      "Ho lavorato su numerosi progetti per clienti di diverse industrie, sempre concentrandomi su soluzioni personalizzate e di alta qualità.",
+    "about-p3":
+      "Il mio background include una solida formazione tecnica e un forte interesse per le tecnologie emergenti.",
+    "cta-linkedin": "Trovami su LinkedIn",
+    "portfolio-title": "Portfolio",
+    "contact-title": "Contatti",
+    "contact-p1":
+      "Se hai domande o desideri collaborare su un progetto, non esitare a contattarmi.",
+    "contact-p2":
+      "Puoi raggiungermi tramite email o telefono. Sarò felice di discutere le tue idee e come possiamo lavorare insieme.",
+    "contact-p3":
+      "Grazie per aver visitato il mio sito. Spero di poter collaborare presto con te.",
+    "cta-title": "Inizia il tuo Progetto Oggi",
+    "cta-text":
+      "Contattami per discutere la tua idea e iniziare a lavorare insieme verso il successo.",
+    "cta-button": "Contattami Ora",
+    "cta-click-project": "Clicca Qui",
+    "nav-home": "Home",
+    "nav-about": "Su di Me",
+    "nav-portfolio": "Portfolio",
+    "nav-contact": "Contatti",
+    "lang-it": "Italiano",
+    "lang-en": "English",
+    "lang-fr": "Français",
+    "footer-text": "© 2024 - Valeria Panzetta. Tutti i diritti riservati.",
+    "contact-form-name": "Nome:",
+    "contact-form-email": "Email:",
+    "contact-form-message": "Messaggio:",
+    "contact-form-submit": "Invia",
+    "thank-you-message": "Grazie! Sarai contattato a breve.",
+  },
+  en: {
+    "home-name": "Hi! I am Valeria Panzetta",
+    "home-title": "Discover My Business Idea",
+    "home-p1":
+      "Innovation and development to help your business grow. Find out how I can help you achieve your goals.",
+    "home-cta": "Explore My Portfolio",
+    "home-business": "Digital Branding Consulting",
+    "home-strategies": "Innovative strategies to take your idea to success",
+    "home-development": "Web Development",
+    "home-solutions": "Cutting-edge digital solutions for your business",
+    "home-marketing": "Digital Marketing",
+    "home-campaigns":
+      "Capture your audience's attention with effective campaigns.",
+    "about-title": "About Me",
+    "about-p1":
+      "I am a professional with years of experience in the field, specializing in web development and front-end design.",
+    "about-p2":
+      "I have worked on numerous projects for clients in various industries, always focusing on customized and high-quality solutions.",
+    "about-p3":
+      "My background includes a solid technical education and a strong interest in emerging technologies.",
+    "portfolio-title": "Portfolio",
+    "cta-linkedin": "Find me on LinkedIn",
+    "contact-title": "Contact",
+    "contact-p1":
+      "If you have any questions or would like to collaborate on a project, feel free to contact me.",
+    "contact-p2":
+      "You can reach me by email or phone. I would be happy to discuss your ideas and how we can work together.",
+    "contact-p3":
+      "Thank you for visiting my website. I hope to collaborate with you soon.",
+    "cta-title": "Start Your Project Today",
+    "cta-text":
+      "Contact me to discuss your idea and start working together towards success.",
+    "cta-button": "Contact Me Now",
+    "cta-click-project": "Click Here",
+    "nav-home": "Home",
+    "nav-about": "About Me",
+    "nav-portfolio": "Portfolio",
+    "nav-contact": "Contact",
+    "lang-it": "Italiano",
+    "lang-en": "English",
+    "lang-fr": "Français",
+    "footer-text": "© 2024 - Valeria Panzetta. All rights reserved.",
+    "contact-form-name": "Name:",
+    "contact-form-email": "Email:",
+    "contact-form-message": "Message:",
+    "contact-form-submit": "Send",
+    "thank-you-message": "Thank you! You will be contacted shortly.",
+  },
+  fr: {
+    "home-name": "Salut ! Je suis Valeria Panzetta",
+    "home-title": "Découvrez Mon Idée d'Entreprise",
+    "home-p1":
+      "Innovation et développement pour aider votre entreprise à croître. Découvrez comment je peux vous aider à atteindre vos objectifs.",
+    "home-cta": "Explorez Mon Portfolio",
+    "home-business": "Consultant en stratégie de branding numérique",
+    "home-strategies": "Stratégies innovantes pour mener votre idée au succès",
+    "home-development": "Développement Web",
+    "home-solutions": "Solutions numériques de pointe pour votre entreprise",
+    "home-marketing": "Marketing numérique",
+    "home-campaigns":
+      "Captez l'attention de votre public grâce à des campagnes efficaces",
+    "about-title": "À propos de moi",
+    "about-p1":
+      "Je suis un professionnel avec des années d'expérience dans le domaine, spécialisé dans le développement web et le design front-end.",
+    "about-p2":
+      "J'ai travaillé sur de nombreux projets pour des clients dans diverses industries, en me concentrant toujours sur des solutions personnalisées et de haute qualité.",
+    "about-p3":
+      "Mon parcours comprend une solide formation technique et un fort intérêt pour les technologies émergentes.",
+    "portfolio-title": "Portfolio",
+    "cta-linkedin": "Retrouve-moi sur LinkedIn",
+    "contact-title": "Contact",
+    "contact-p1":
+      "Si vous avez des questions ou souhaitez collaborer sur un projet, n'hésitez pas à me contacter.",
+    "contact-p2":
+      "Vous pouvez me joindre par email ou par téléphone. Je serais heureux de discuter de vos idées et de la manière dont nous pourrions travailler ensemble.",
+    "contact-p3":
+      "Merci de visiter mon site web. J'espère collaborer avec vous bientôt.",
+    "cta-title": "Commencez Votre Projet Aujourd'hui",
+    "cta-text":
+      "Contactez-moi pour discuter de votre idée et commencer à travailler ensemble vers le succès.",
+    "cta-button": "Contactez-moi Maintenant",
+    "cta-click-project": "Cliquez Ici",
+    "nav-home": "Accueil",
+    "nav-about": "À propos de moi",
+    "nav-portfolio": "Portfolio",
+    "nav-contact": "Contact",
+    "lang-it": "Italiano",
+    "lang-en": "English",
+    "lang-fr": "Français",
+    "footer-text": "© 2024 - Valeria Panzetta. Tous droits réservés.",
+    "contact-form-name": "Nom:",
+    "contact-form-email": "Email:",
+    "contact-form-message": "Message:",
+    "contact-form-submit": "Envoyer",
+    "thank-you-message": "Merci! Vous serez contacté sous peu.",
+  },
+};
+
+// Contact Form Handling
+const contactForm = document.getElementById("contactForm");
+if (contactForm) {
+  contactForm.addEventListener("submit", function (event) {
+    event.preventDefault(); 
+
+    const name = document.getElementById("name").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const message = document.getElementById("message").value.trim();
+
+    if (name !== "" && email !== "" && message !== "") {
+      // Mostra il messaggio di ringraziamento
+      document.getElementById("thankYouMessage").style.display = "block";
+
+
+      // Reset module after message was sent
+      contactForm.reset();
+    }
+  });
+}
+
+// Change language function
+function changeLanguage(language) {
+  const elements = document.querySelectorAll("[data-translate-key]");
+  elements.forEach((element) => {
+    const key = element.getAttribute("data-translate-key");
+    if (translations[language] && translations[language][key]) {
+      if (element.tagName === "IMG") {
+        element.setAttribute("alt", translations[language][key]);
+      } else if (
+        element.tagName === "INPUT" ||
+        element.tagName === "TEXTAREA"
+      ) {
+        element.setAttribute("placeholder", translations[language][key]);
+      } else if (element.tagName === "OPTION") {
+        element.textContent = translations[language][key];
+      } else {
+        element.textContent = translations[language][key];
+      }
+    } else {
+      console.warn(`Missing translation for ${key} in ${language}`);
+    }
+  });
+}
+
+// Change Language Event listener
+document
+  .getElementById("language-switcher")
+  .addEventListener("change", (event) => {
+    const selectedLanguage = event.target.value;
+    changeLanguage(selectedLanguage);
+    localStorage.setItem("selectedLanguage", selectedLanguage);
+  });
+
+// Initial language - Italian
+window.addEventListener("DOMContentLoaded", () => {
+  const savedLanguage = localStorage.getItem("selectedLanguage") || "it";
+  changeLanguage(savedLanguage);
+  document.getElementById("language-switcher").value = savedLanguage;
+});
